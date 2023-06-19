85bf928f35844f5b0828b698badbd73f44393ad3
Fernanda <fernanda_petrachim@hotmail.com>
2023-06-18 21:19:28 -0300

    ainda no 2

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import searchAlbumsAPI from './services/searchAlbumsAPI';
 import Loading from './Loading';
+import { Link } from 'react-router-dom';
+
+interface Album {
+  collectionId: string;
+  collectionName: string;
+}
 
 function Search() {
   const [nomeArtista, setNomeArtista] = useState<string>('');
   const [loading, setLoading] = useState(false);
-  const [albuns, setAlbuns] = useState([]);
+  const [albuns, setAlbuns] = useState<Album[]>([]);
 
   const handleChangeArtista = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     setNomeArtista(event.target.value);
   };
+
   const handleClickPesquisar = async (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
     setLoading(true);
@@ -19,14 +26,16 @@ function Search() {
     setLoading(false);
     setAlbuns(resposta);
   };
+
   if (loading) {
     return <Loading />;
   }
+
   return (
     <>
       <p>Testando</p>
       <input
-        onChange={ handleChangeArtista }
+        onChange={handleChangeArtista}
         type="text"
         data-testid="search-artist-input"
         placeholder="Digite algo"
@@ -34,24 +43,21 @@ function Search() {
       <button
         type="button"
         data-testid="search-artist-button"
-        disabled={ nomeArtista.length < 2 }
-        onClick={ handleClickPesquisar }
+        disabled={nomeArtista.length < 2}
+        onClick={handleClickPesquisar}
       >
         Pesquisar
       </button>
-      {
-      albuns.map((album) => {
-        return (
+      {albuns.map((album) => (
+        <div key={album.collectionId}>
+          <Link to={`/album/${album.collectionId}`} data-testid={`link-to-album-${album.collectionId}`}>
+            <h3>{album.collectionName}</h3>
+          </Link>
           <ol>
-<li>
-  {
-    album.collectionName
-  }
-</li>
+            <li>{album.collectionName}</li>
           </ol>
-        )
-      });
-      }
+        </div>
+      ))}
     </>
   );
 }

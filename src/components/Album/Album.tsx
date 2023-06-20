322e31c8f6719d0b603e16c1c6fe4812426c2b38
Fernanda <fernanda_petrachim@hotmail.com>
2023-06-20 10:36:32 -0300

    terminado a 3 com auxilio da mentoria, do Ramon

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -5,18 +5,13 @@ import MusicCard from './Music.Card';
 import Loading from '../../Loading';
 import { AlbumType, SongType } from '../../types';
 
-const DEFAULT_TYPE = {
-  trackId: 0,
-  trackName: '',
-  previewUrl: '',
-
-};
 function Album() {
   const [loading, setLoading] = useState(true);
-  const [music, setMusic] = useState<AlbumType[] | null>(null); // musica primeiro estado dele, a segunda é uma função que altera o estado
+  const [music, setMusic] = useState<SongType[]>(); // musica primeiro estado dele, a segunda é uma função que altera o estado
   // guardar o array retorno,guardar as musicas
   // ele começa com array vazio
   // set de setar
+  const [colecao, setColecao] = useState<AlbumType | null>();
   const { id } = useParams();
   useEffect(() => {
     const fetchMusicas = async () => {
@@ -24,12 +19,14 @@ function Album() {
       if (id) {
         const musicas = await getMusics(id); // getmusic para buscar as listas de músicas e coloquei no estado music
         /*    const index = musicas.findIndex((song) => song.trackId === song); */
-        setMusic(musicas); // um array de lista de musicas, setou p estado de music com array de objetos
+        const Collection = musicas[0];
+        setMusic(musicas.slice(1) as SongType[]); // um array de lista de musicas, setou p estado de music com array de objetos
+        setColecao(Collection);
         setLoading(false);
       }
     };
     fetchMusicas();
-  }, []);
+  }, [id]);
 
   if (loading) {
     return (
@@ -41,10 +38,10 @@ function Album() {
 
   return (
     <>
-      <h1 data-testid="album-name">Collection Name</h1>
-      <h2 data-testid="artist-name">Artist Name</h2>
+      <h1 data-testid="album-name">{ colecao?.collectionName }</h1>
+      <h2 data-testid="artist-name">{ colecao?.artistName }</h2>
       {music && music.length > 0
-        ? music.slice(1)
+        ? music
           .map((song, index) => (<MusicCard key={ index } music={ song } />))
         : (
           <p>Não tem essas músicas disponíveis</p>
